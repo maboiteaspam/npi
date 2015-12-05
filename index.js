0eaf82aeefa5ccf607595859f0ec566e0dd97f9a
clement <maboiteaspam@wanadoo.fr>
2015-12-05 20:30:46 +0800

    refactored template generation

--- index.js
+++ index.js
@@ -11,36 +11,31 @@ if (argv.verbose || argv.v) process.env['DEBUG'] = [
 require('console.md')();
 
 var path          = require('path')
-var fs            = require('fs')
-var _             = require('lodash')
 var eventStream   = require('event-stream-writer')
 var messageRouter = require('stream-message-router')
 var spawn         = require('./lib/spawn')
 var touch         = require('./lib/touch')
 var push          = require('./lib/push')
 var extract       = require('./lib/extract')
+var genTemplate   = require('./lib/generatetemplate')
 var pipeSpawned   = require('./lib/pipespawned')
 var npmInstall    = require('./lib/npminstall')
 
 
 var pkg     = require('./package.json')
+var files   = [];
 var ignored = [
   'node_modules/',
   'npm-debug.log'
 ];
-var files = [];
 
-var templateVars = {
+var tplPath       = path.join(__dirname, 'template');
+var templateVars  = {
   name        : path.basename(process.cwd()),
   description : "Description of the module.",
   ignored     : ignored,
   modules     : argv['_']
 };
-
-var README      = generateTemplate('README.md.ejs', templateVars);
-var playground  = generateTemplate('playground.js.ejs', templateVars);
-var index       = generateTemplate('index.js.ejs', templateVars);
-var gitIgnore   = generateTemplate('.gitignore.ejs', templateVars);
 
 
 console.log('npi %s', pkg.version)
@@ -50,10 +45,10 @@ npi
   .pipe(touch('package.json'))
   .pipe(spawn('npm', ['init', '--yes'], {stdio: 'pipe'}))
   .pipe(spawn('git', ['init'], {stdio: 'pipe'}))
-  .pipe(touch('README.md', README))
-  .pipe(touch('index.js', index))
-  .pipe(touch('.gitignore', gitIgnore))
-  .pipe(touch('playground.js', playground))
+  .pipe(genTemplate(tplPath, 'README.md'    , templateVars))
+  .pipe(genTemplate(tplPath, 'playground.js', templateVars))
+  .pipe(genTemplate(tplPath, 'index.js'     , templateVars))
+  .pipe(genTemplate(tplPath, '.gitignore'   , templateVars))
   .pipe(npmInstall(argv['_']))
   .pipe(spawn('git', function (){
     return ['add'].concat(files)
@@ -87,9 +82,3 @@ npi.write({
   message : 'npi',
   body    : argv
 });
-
-function generateTemplate (file, vars) {
-  return _.template(
-    fs.readFileSync(path.join(__dirname, 'template', file))
-  )(vars);
-}
